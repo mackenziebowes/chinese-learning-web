a81212a192a95d17c681aec1ee3f0f5c6e1c8f7b
Mackenzie Bowes <mackenzie@mackenziebowes.com>
2026-02-10 14:22:03 -0800

    I hate python

--- macks_python/src/mandarin_app/audio/__init__.py
+++
@@ -1,2 +0,0 @@
-from .engine import AudioEngine
-from .processor import PitchProcessor
--- macks_python/src/mandarin_app/audio/engine.py
+++
@@ -1,6 +0,0 @@
-class AudioEngine:
-    def __init__(self, rate=44100):
-        self.rate = rate
-
-    def start_stream(self):
-        print("Stream started")
